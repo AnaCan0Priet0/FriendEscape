743e56012b76b9da2635d94f2d14022d29fa2bec
AnaCan0Priet0 <programadoraenapuros@gmail.com>
2020-04-08 10:06:10 +0200

    updated nodemailer

--- friendescape-api/logic/join-group.js
+++ friendescape-api/logic/join-group.js
@@ -1,8 +1,10 @@
 const { models: { Group, User } } = require('friendescape-data')
 const { validate } = require('friendescape-utils')
-const { NotFoundError, NotAllowedError } = require('friendescape-errors')
+const { NotFoundError} = require('friendescape-errors')
+const nodemailer = require('nodemailer')
 
 module.exports = (userId, groupId) => {
+
     validate.string(userId, 'userId')
     validate.string(groupId, 'groupId')
 
@@ -14,4 +16,29 @@ module.exports = (userId, groupId) => {
             return Promise.all([User.findByIdAndUpdate(userId, { $addToSet: { subbedTo: groupId } }), Group.findByIdAndUpdate(groupId, { $addToSet: { subevents: userId } })])
         })
         .then(() => { })
+        .then(() => {
+            transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'programadoraenapuros@gmail.com',
+                    pass: 'SkyLabProyecto'
+                }
+            })
+            
+                mailOptions = {
+                    from: 'FriendEscape',
+                    to: `anacano1985@gmail.com`,
+                    subject: 'Te has añadido a un grupo',
+                    text: `You have booked`,
+              }
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+              
+            })
+        })
+        .then(() => {})
 }
