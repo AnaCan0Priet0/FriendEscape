bbddadf6d6c0998cb90ce1bd2d5fb5e54a1181f0
AnaCan0Priet0 <programadoraenapuros@gmail.com>
2020-04-09 10:41:20 +0200

    fixed errors

--- friendescape-api/logic/join-group.js
+++ friendescape-api/logic/join-group.js
@@ -3,28 +3,47 @@ const { validate } = require('friendescape-utils')
 const { NotFoundError} = require('friendescape-errors')
 const nodemailer = require('nodemailer')
 
+
+
 module.exports = (userId, groupId) => {
 
     validate.string(userId, 'userId')
-    validate.string(groupId, 'groupId')
+    validate.string(groupId, 'groupId') 
 
     return Promise.all([User.findById(userId), Group.findById(groupId)])
         .then(([user, group]) => {
             if (!user) throw new NotFoundError(`user with id ${userId} does not exist`)
             if (!group) throw new NotFoundError(`group with id ${groupId} does not exist`)
-
-            return Promise.all([User.findByIdAndUpdate(userId, { $addToSet: { subbedTo: groupId } }), Group.findByIdAndUpdate(groupId, { $addToSet: { subevents: userId } }).populate('subevents', 'name surname email').populate('escapeRoom', 'title location theme difficulty duration price')])
+            console.log(user)
+            if (group.subevents.includes(userId))throw new NotFoundError (`this user is already on the group`)
+            console.log(group)
+            return Promise.all([User.findByIdAndUpdate(userId, { $addToSet: { subbedTo: groupId } }), Group.findByIdAndUpdate(groupId, { $addToSet: { subevents: userId } }).populate('subevents', 'name surname email').populate('escapeRoom', 'title location theme difficulty duration price minplayers maxplayers')])
+            
         })
+        
         //.then(() => { })
         .then(([user, group]) => {
             //TODO modify group subevents. palabra del profesor
-            const {date, time, escapeRoom :{title, location, theme, difficulty, duration, price}} = group
-            const body = `Congrats, you joined the next group:,
+            const {date, time, escapeRoom :{title, location, theme, difficulty, duration, price, minplayers, maxplayers}} = group
+            const {email} = user
+            const body = `Hi friend, 
+            Here do you have the information about your next appointment:
+            Escape Room: ${title},
+            Theme: ${theme},
             Date: ${date},
             Time: ${time},
             Location: ${location},
-            Group Members: ${group.subevents}
-            you have an appointment next escaperoom ${title}, ${theme}, ${difficulty}, ${duration}, ${price}`
+            Group Members: ${group.subevents},
+            Aditional information: 
+            Difficulty: ${difficulty}, 
+            Duration: ${duration}, 
+            Price: ${price},
+            Min-players:${minplayers},
+            Max-players:${maxplayers},
+            If you have any issue please, don't hesitate to contact us:
+            Email: friendescape@friendescape.com
+            Office: 9 Land Street Toowong, Brisbane (8-18h)`
+            
             transporter = nodemailer.createTransport({
                 service: 'gmail',
                 auth: {
@@ -35,9 +54,14 @@ module.exports = (userId, groupId) => {
             
                 mailOptions = {
                     from: 'FriendEscape',
-                    to: `anacano1985@gmail.com`,
-                    subject: 'Te has añadido a un grupo',
+                    to: `${email}`,
+                    subject: 'Congrats, you joined a group',
                     text: body
+                    // attachments: [
+                    //     { filename: 'fear.jpg',
+                    //     path: './fear.jpg'}
+                    // ]
+
               }
                 transporter.sendMail(mailOptions, function (error, info) {
                     if (error) {
